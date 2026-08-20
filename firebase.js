304f0453a9d75ffb9bae99d0b1e4e6e2e3d54860
Jaganiketan <jaganiketan2@gmail.com>
2026-08-20 22:35:00 +0530

    Refactor Firebase configuration and exports
    Updated Firebase API key and refactored export statements.

--- firebase.js
+++ firebase.js
@@ -9,10 +9,9 @@ import {
   getAuth
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-
 // Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyBe1Gi-atO4ugqIIHNs5W_8x6DH0oCY9g",
+  apiKey: "AIzaSyBe1Gi-atOr6ugqIIHNs5W_8x6DH0oCY9g",
   authDomain: "kayraenterprise-8a2ec.firebaseapp.com",
   projectId: "kayraenterprise-8a2ec",
   storageBucket: "kayraenterprise-8a2ec.firebasestorage.app",
@@ -20,26 +19,12 @@ const firebaseConfig = {
   appId: "1:541311529043:web:d8e2300b7290e7caa356a6"
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
-// Firestore Database
 const db = getFirestore(app);
-
-
-// Firebase Authentication
 const auth = getAuth(app);
 
-
-// Export
-export {
-  app,
-  db,
-  auth
-};
-
+export { app, db, auth };
 
 console.log("✅ Firebase Connected");
-console.log("✅ Firebase Authentication Connected");
